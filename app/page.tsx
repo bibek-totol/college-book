0d8133fbae07f72030093330e9106e51b0d06dfe
bibek-totol <bbibekbhowmick2001@gmail.com>
2025-11-13 10:11:57 +0600

    Final Touch

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,9 @@ const Home = () => {
         </motion.div>
 
         
-        <motion.div
-          initial={{ opacity: 0, y: 100, scale: 0.95 }}
-          whileInView={{ opacity: 1, y: 0, scale: 1.10 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-          viewport={{ once: false, amount: 0.1 }}
-        >
+       
           <ReviewsSection />
-        </motion.div>
+        
       </main>
     </div>
   );
